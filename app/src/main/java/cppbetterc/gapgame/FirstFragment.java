b31a1dbb57c9f37f2796c1f40ad45cea33aabba0
Simpson <charlie40503@gmail.com>
2017-04-30 17:12:36 +0800

    action bar add button

---
+++ app/src/main/java/cppbetterc/gapgame/FirstFragment.java
@@ -0,0 +1,23 @@
+package cppbetterc.gapgame;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Simpson on 2017/4/30.
+ */
+
+public class FirstFragment extends Fragment {
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.firstlayout, container, false);
+        return myView;
+    }
+}
